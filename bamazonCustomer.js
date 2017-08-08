7e02d59ec7921b3125d423f6de4046d9a1554f6f
Gildon Kirk <gildonkirk@gmail.com>
2017-08-08 12:22:05 -0400

    prints item id name and price to console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,19 @@
 var mysql = require('mysql');
 
-var connection = sql.createConnection({
+var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
 	user: 'root',
 	password: 'certifiedG48*',
 	database: 'bamazon'
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	connection.query('SELECT * FROM products ', function(err, res) {
+		if (err) throw err;
+		for(i = 0; i < res.length; i++) {
+			console.log('ID: ' + res[i].item_id + ' | ' + res[i].product_name + ' | $' + res[i].price);
+		};
+	});
 });
